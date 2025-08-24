b8487ce5c801a76fac7de864e4bcd1351ec9376f
preetikumari15 <annukumari0928@gmail.com>
2025-08-24 20:13:38 +0530

    functionality check

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -80,9 +80,9 @@ const Navbar = () => {
               Logout
             </button>
           ) : (
-            <Link to="/signup" onClick={() => setMenuOpen(false)}>
+            <Link to="/login" onClick={() => setMenuOpen(false)}>
               <button className="px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 text-white transition-all duration-200">
-                Register
+                Login
               </button>
             </Link>
           )}
